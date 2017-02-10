4883ac1cb352bd7bb52941286d7c4f151416fbc3
AlexZ188 <az@79element.com>
2017-02-10 23:07:06 +0800

    9th commit 52-2 question answer counters

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   def create
     @user = User.authenticate(params[:email], params[:password])
 
-    if @user.present?
-      session[:user_id] = @user.id
+    if user.present?
+      session[:user_id] = user.id
       redirect_to root_url, notice: 'Вы успешно залогинены'
     else
       flash.now.alert = 'Неправильный мейл или пароль'
